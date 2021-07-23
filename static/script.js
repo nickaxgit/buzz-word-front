9ae26f2b39808761628a4187480bfc343aa434e9
Nick Axworthy <nickax@gmail.com>
2021-07-23 19:33:05 +0100

    Set button type to button

--- static/script.js
+++ static/script.js
@@ -56,6 +56,7 @@ async function getState() {
         state.gameRooms.forEach(r => {
 
             let roomButton = document.createElement('button')
+            roomButton.attributes("type")="button"
             roomButton.classList.add('btnRooms')
             roomButton.classList.add(r.roomName.toLowerCase())
             roomList.appendChild(roomButton)
